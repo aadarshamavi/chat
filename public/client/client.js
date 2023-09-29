4258992f4d4949b365afb569b23a6c5a9e2ee92f
aadarshamavi <138198967+aadarshamavi@users.noreply.github.com>
2023-09-29 10:55:52 +0545

    Updated to store messages

--- public/client/client.js
+++ public/client/client.js
@@ -1,56 +1,75 @@
-const socket = io()
-let textarea = document.querySelector("textarea")
+const socket = io();
+let textarea = document.querySelector("textarea");
 let main = document.querySelector("main");
-let username = Math.random().toString(32).split(".")[1]
-textarea.addEventListener("keyup",(e)=>{
-    let data = {
-        username,
-        message:e.target.value
-    }
-    if(e.key=="Enter")
-    {
-    
-        addMessages(data,"sent")
-        socket.emit("send-message",data)
-        e.target.value=""
-    }
-})
-socket.on("new-messages",(data)=>{
-    addMessages(data,"received")
-})
-function addMessages(data,type){
-let p =document.createElement("p")
-p.innerText = data.username
-let h3 = document.createElement("h3")
-h3.innerText = data.message
-let messageDiv = document.createElement("div")
-messageDiv.classList.add("message")
-messageDiv.classList.add(type);
-messageDiv.appendChild(p)
-messageDiv.appendChild(h3)
-main.appendChild(messageDiv)
-scrollBottom()
-}
 
-function scrollBottom(){
-    main.scrollTop = main.scrollHeight
+let username = localStorage.getItem("username");
+const userId =
+  localStorage.getItem("userId") ||
+  localStorage.getItem(
+    "userId",
+    localStorage.setItem("userId", Math.random().toString(32).split(".")[1])
+  );
+textarea.addEventListener("keyup", (e) => {
+  let data = {
+    username,
+    message: e.target.value.trim(),
+    userId,
+  };
+  if (e.key == "Enter" && data.message !== " ") {
+    addMessages(data, "sent");
+    socket.emit("send-message", data);
+    e.target.value = "";
+  }
+});
+socket.on("new-messages", (data) => {
+  addMessages(data, "received");
+});
+function addMessages(data, type) {
+  let p = document.createElement("p");
+  p.innerText = data.username;
+  let h3 = document.createElement("h3");
+  h3.innerText = data.message;
+  let messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+  messageDiv.classList.add(type);
+  messageDiv.appendChild(p);
+  messageDiv.appendChild(h3);
+  main.appendChild(messageDiv);
+  scrollBottom();
 }
-function addUsername(){
-    let user = document.getElementById("username").value
-    if(user.length<3){
-        alert("Minimun 3 letters are required")
-    }else{
-        username = user
-        socket.emit("new-user-connected",username)
-        let dialog = document.querySelector("dialog")
-        dialog.removeAttribute("open")
-    }
-   
+function scrollBottom() {
+  main.scrollTop = main.scrollHeight;
+}
+function addUsername() {
+  let user = document.getElementById("username").value;
+  if (user.length < 3) {
+    alert("Minimun 3 letters are required");
+  } else {
+    username = user;
+    socket.emit("new-user-connected", username);
+    let dialog = document.querySelector("dialog");
+    dialog.removeAttribute("open");
+    localStorage.setItem("username", user);
+  }
+}
+if (username) {
+  let dialog = document.querySelector("dialog");
+  dialog.removeAttribute("open");
+}
+socket.on("welcome-user", (userName) => {
+  let p = document.createElement("p");
+  p.innerText = userName + " joined the chat!";
+  p.classList.add("user-entered");
+  main.appendChild(p);
+});
 
-}
-socket.on("welcome-user",(userName)=>{
-    let p = document.createElement("p")
-    p.innerText=userName+" joined the chat!"
-    p.classList.add("user-entered")
-    main.appendChild(p)
-})
+socket.on("olderMessages", (messages) => {
+  console.log(messages);
+  messages.forEach((message) => {
+    if (message.userId == userId) {
+      addMessages(message, "sent");
+    } else {
+      addMessages(message, "received");
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const express = require("express")
-const app = express()
-const http = require("http")
-const server = http.Server(app)
-const io = require("socket.io")(server)
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.Server(app);
+const io = require("socket.io")(server);
 
-app.use(express.static(__dirname+"/public"))
+let users = [];
+let messages = [];
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
-})
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-io.on("connection",(socket)=>{
-    console.log("User connected")
-    socket.on("send-message",(data)=>{
-        console.log(data)
-        socket.broadcast.emit("new-messages",data)
-    })
-    socket.on("new-user-connected",username=>{
-        socket.broadcast.emit("welcome-user",username)
-    })
-})
+io.on("connection", (socket) => {
+  users.push(socket.id);
 
-server.listen(3000)
+  socket.emit("olderMessages", messages);
+  socket.on("send-message", (data) => {
+    messages.push(data);
+    socket.broadcast.emit("new-messages", data);
+  });
+
+  socket.on("new-user-connected", (username) => {
+    socket.broadcast.emit("welcome-user", username);
+  });
+  socket.on("disconnect", () => {
+    users = users.filter((id) => id !== socket.id);
+    if (users.length == 0) {
+      messages = [];
+    }
+  });
+});
+
+server.listen(3000);
